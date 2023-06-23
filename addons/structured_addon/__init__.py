e53692f9c49975dcb7424d9703f5b8f69707ed75
Samuele Amisano <65444098+Samu-Amy@users.noreply.github.com>
2023-06-23 19:47:59 +0200

    Primo upload

---
+++ addons/structured_addon/__init__.py
@@ -0,0 +1,25 @@
+import bpy
+
+bl_info = {
+    "name": "A Structured Add-on",
+    "description": "Add-on consisting of multiple files",
+    "author": "Samu Amy",
+    "version": (1, 0),
+    "blender": (3, 5, 0),
+    "location": "View3D",
+    "warning": "This addon is still in development.",
+    "wiki_url": "",
+    "category": "Learning"
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == "__main__":
+    register()
